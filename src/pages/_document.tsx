4fae67263cb04f30e9baab38a70dfe0bba72f2c3
Louise Bawas <louisebawas@MacBook-Pro-2.local>
2023-06-01 17:33:01 +0900

    change name

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,17 +4,17 @@ export default function Document() {
   return (
     <Html>
       <Head>
-      <meta name="application-name" content="PWA Test App" />
+        <meta name="application-name" content="MikoSea" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="PWA App" />
-        <meta name="description" content="Best PWA App in the world" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="MikoSea" />
+        <meta name="description" content="支援型NFTプラットフォーム" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/icons/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#2B5797" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#ffffff" />
 
         <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
