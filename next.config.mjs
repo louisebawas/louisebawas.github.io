469138249c0ec9ce03ad8d7e6a5dd5d51f30e129
Louise Bawas <louisebawas@MacBook-Pro-2.local>
2023-06-01 17:00:31 +0900

    add output:export to next-config

--- next.config.mjs
+++ next.config.mjs
@@ -28,15 +28,16 @@ const config = {
   reactStrictMode: true,
   assetPrefix,
   basePath,
+  output: "export",
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
    * out.
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
 };
 export default withPWA(config);
